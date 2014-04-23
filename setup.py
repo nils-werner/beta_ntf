aeafe68a25df2d27e89daacb10d7c10a82da8281
Nils Werner <nils.werner@audiolabs-erlangen.de>
2014-04-23 10:20:27 +0200

    Wrote setuptools wrapper

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='beta_ntf',
+    version='0.1',
+    description='Super beta-NTF 2000',
+    url='https://code.google.com/p/beta-ntf/',
+    author='Antoine Liutkus',
+    author_email='antoine@liutkus.net',
+    install_requires=['NumPy>=1.6.0'],
+    license='LGPL',
+    packages=['beta_ntf'],
+    zip_safe=True
+)
